6cc45c2f958a3c38baeae6d0bd890c3526d1d7b7
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-05-04 08:02:15 +0200

    Removed useless code.

--- builder.js
+++ builder.js
@@ -4,25 +4,21 @@ var ProtoBuf = require("protobufjs"),
     path = require("path"),
     jsonfile = require('jsonfile');
 
-
 var output_folder = './json_output'
+
 
 var builder = ProtoBuf.newBuilder({convertFieldsToCamelCase: true});
 ProtoBuf.loadProtoFile(path.resolve(process.argv[3]), builder);
 var message = builder.build("edu.psu.cse.siis.ic3.Application");
 
 var buffer = fs.readFileSync(process.argv[2]);
-
 var mymsg = message.decode(buffer);
-
 
 if (!fs.existsSync(output_folder)){
     fs.mkdirSync(output_folder);
 }
-var output = output_folder + '/' + path.basename(process.argv[2], '.dat') + '.json'
-
-
-//fs.writeFileSync(output, util.inspect(mymsg, showHidden=false, depth=null, colorize=true) , 'utf-8');
+var output = output_folder + '/' +
+                path.basename(process.argv[2], '.dat') + '.json'
 
 jsonfile.writeFile(output, mymsg,  {spaces: 4}, function (err) {
     if (err) {
