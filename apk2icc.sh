6091a0dbe22a2ac5d816473c3979d798e67faa05
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-12-06 13:42:08 +0100

    Added script that generated only a IC3 output (binary format).

---
+++ apk2icc.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+# This script extracts information from an Android application (APK file).
+# The output is a JSON file with the components, intents, intents filters
+# and permissions of the application.
+#
+#  $ ./apk2icc.sh ~/sample-APK/application.apk
+#
+# Author: Cédric Bonhomme
+
+APK_FILE=`realpath $1`
+APK_NAME=`basename ${APK_FILE%.apk}`
+
+TEMP_DIRECTORY=`realpath "./tmp"`
+DARE_OUTPUT_DIRECTORY=$TEMP_DIRECTORY"/dare/"$APK_NAME
+IC3_OUTPUT_DIRECTORY=$TEMP_DIRECTORY"/ic3"
+
+# Paths to dare and ic3 binaries
+DARE_DIRECTORY="./dare"
+IC3_DIRECTORY="./ic3"
+
+ANDROID_JAR=`realpath $IC3_DIRECTORY/android.jar`
+
+
+# Generation of the retargeted APK
+$DARE_DIRECTORY/dare -d $DARE_OUTPUT_DIRECTORY $APK_FILE
+
+
+# Generation of the binary proto file
+if [ ! -d "$IC3_OUTPUT_DIRECTORY" ]; then
+  mkdir $IC3_OUTPUT_DIRECTORY
+fi
+if [ ! -d $IC3_OUTPUT_DIRECTORY/$APK_NAME ]; then
+  mkdir $IC3_OUTPUT_DIRECTORY/$APK_NAME
+fi
+
+java -jar $IC3_DIRECTORY/ic3-0.2.0-full.jar \
+    -apkormanifest $APK_FILE \
+    -input $DARE_OUTPUT_DIRECTORY/retargeted/$APK_NAME \
+    -cp $ANDROID_JAR \
+    -protobuf $IC3_OUTPUT_DIRECTORY/$APK_NAME \
+    -binary
+rm -Rf $DARE_OUTPUT_DIRECTORY sootOutput
