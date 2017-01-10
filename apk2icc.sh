eedfcc1983bd5e0e64dd68d0ab6e65d57374e7ee
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-01-10 08:49:14 +0100

    Improvements of the managements of variables.

--- apk2icc.sh
+++ apk2icc.sh
@@ -6,36 +6,34 @@
 #
 #  $ ./apk2icc.sh ~/sample-APK/application.apk
 
+ROOT_APPLICATION="./"
+ROOT_OUTPUT="./result/"
+
+# Paths to dare and ic3 binaries
+DARE_DIRECTORY=$ROOT_APPLICATION"/dare"
+IC3_DIRECTORY=$ROOT_APPLICATION"/ic3"
+ANDROID_JAR=`readlink -f $IC3_DIRECTORY/android.jar`
+
+# APK file to analyze
 APK_FILE=`realpath $1`
 APK_NAME=`basename ${APK_FILE%.apk}`
 
-TEMP_DIRECTORY=`realpath "./tmp"`
-DARE_OUTPUT_DIRECTORY=$TEMP_DIRECTORY"/dare/"$APK_NAME
-IC3_OUTPUT_DIRECTORY="./result"
-
-# Paths to dare and ic3 binaries
-DARE_DIRECTORY="./dare"
-IC3_DIRECTORY="./ic3"
-
-ANDROID_JAR=`realpath $IC3_DIRECTORY/android.jar`
-
+# Outputs
+DARE_OUTPUT_DIRECTORY=$ROOT_OUTPUT"/dare/"$APK_NAME
+mkdir -p $DARE_OUTPUT_DIRECTORY
+DARE_OUTPUT_DIRECTORY=`readlink -f $DARE_OUTPUT_DIRECTORY`
+IC3_OUTPUT_DIRECTORY=$ROOT_OUTPUT"/ic3/"$APK_NAME
+mkdir -p $IC3_OUTPUT_DIRECTORY
+IC3_OUTPUT_DIRECTORY=`readlink -f $IC3_OUTPUT_DIRECTORY`
 
 # Generation of the retargeted APK
 $DARE_DIRECTORY/dare -d $DARE_OUTPUT_DIRECTORY $APK_FILE
 
-
 # Generation of the binary proto file
-if [ ! -d "$IC3_OUTPUT_DIRECTORY" ]; then
-  mkdir $IC3_OUTPUT_DIRECTORY
-fi
-if [ ! -d $IC3_OUTPUT_DIRECTORY/$APK_NAME ]; then
-  mkdir $IC3_OUTPUT_DIRECTORY/$APK_NAME
-fi
-
 java -jar $IC3_DIRECTORY/ic3-0.2.0-full.jar \
     -apkormanifest $APK_FILE \
     -input $DARE_OUTPUT_DIRECTORY/retargeted/$APK_NAME \
     -cp $ANDROID_JAR \
-    -protobuf $IC3_OUTPUT_DIRECTORY/$APK_NAME \
+    -protobuf $IC3_OUTPUT_DIRECTORY \
     -binary
-rm -Rf $DARE_OUTPUT_DIRECTORY sootOutput $TEMP_DIRECTORY
+rm -Rf $DARE_OUTPUT_DIRECTORY sootOutput
